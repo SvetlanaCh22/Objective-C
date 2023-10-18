5151ff46fc7f2283a542a8567dd44b5a74308ea4
SvetlanaCh22 <111069682+SvetlanaCh22@users.noreply.github.com>
2023-10-19 00:47:58 +0300

    Add files via upload

---
+++ hw1/solve_equation.m
@@ -0,0 +1,27 @@
+﻿#import <Foundation/Foundation.h>
+
+void solveQuadraticEquation(float a, float b, float c) {
+    float discriminant = b * b - 4 * a * c;
+    
+    if (discriminant > 0) {
+        float x1 = (-b + sqrt(discriminant)) / (2 * a);
+        float x2 = (-b - sqrt(discriminant)) / (2 * a);
+        
+        NSLog(@"Уравнение имеет два корня: x1 = %f, x2 = %f", x1, x2);
+    } else if (discriminant == 0) {
+        float x = -b / (2 * a);
+        
+        NSLog(@"Уравнение имеет один корень: x = %f", x);
+    } else {
+        NSLog(@"Уравнение не имеет действительных корней");
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        solveQuadraticEquation(1, -8, 12);
+        solveQuadraticEquation(12, -4, 2);
+        solveQuadraticEquation(1, -100, -2);
+    }
+    return 0;
+}
